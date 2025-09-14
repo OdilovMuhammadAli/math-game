cda1d79561104897dd57d37edc3a900d59ebae84
MuhammadAli <MuhammadAli>
2025-09-14 18:03:42 +0500

    add new math

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import { mathSigns } from "./constants.js";
+import { getRandomNumber, getRandomNumberArray } from "./functions.js";
+
+function init() {
+  let a = getRandomNumber(9);
+  let b = getRandomNumber(9);
+  const signIndex = getRandomNumber(mathSigns.length - 1);
+  const sign = mathSigns[signIndex];
+
+  if (sign === "/") {
+    while (a < b || a % b !== 0) {
+      a = getRandomNumber(9);
+      b = getRandomNumber(9);
+    }
+  }
+
+  const expression = `${a}${sign}${b}`;
+  let result = 0;
+  eval(`result = ${expression}`);
+  console.log(result);
+
+  console.log(getRandomNumberArray(16, result));
+}
+init();
---
+++ js/functions.js
@@ -0,0 +1,24 @@
+export function getRandomNumber(num) {
+  const result = Math.trunc(Math.random() * num + 1);
+  return result;
+}
+
+export function getRandomNumberArray(num, res) {
+  const result = [];
+
+  while (true) {
+    let randomNum = getRandomNumber(99);
+    while (randomNum === res) {
+      randomNum = getRandomNumber(99);
+    }
+    result.push(randomNum);
+    if (result.length === num) break;
+  }
+  return result;
+}
+
+export function readyRandomNumbers(array, res) {
+  const randomIndex = getRandomNumber(array.length - 1);
+  array[randomIndex] = res;
+  return array;
+}
